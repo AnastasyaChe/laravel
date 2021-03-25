34bdc1656a63a9dc6a30f7122d474ed390b5acc8
Anastasya <bepin@yandex.ru>
2021-03-25 10:37:50 +0300

    hw1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/intro', function () {
+    return view('introduction');
+});
+Route::get('/tanya', function () {
+    return view('tatiana');
+});
